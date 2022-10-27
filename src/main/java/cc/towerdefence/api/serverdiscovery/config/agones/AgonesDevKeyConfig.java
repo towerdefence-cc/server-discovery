1efff6f6ee4aaf2dad9425c28711c1758b3b70c4
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-10-27 03:53:47 +0100

    No im not happy about doing this.

--- src/main/java/cc/towerdefence/api/serverdiscovery/config/agones/AgonesDevKeyConfig.java
+++ src/main/java/cc/towerdefence/api/serverdiscovery/config/agones/AgonesDevKeyConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.nio.file.Path;
 
@@ -23,6 +24,23 @@ public class AgonesDevKeyConfig {
     @Bean
     public ChannelCredentials sslContext() throws IOException {
         return TlsChannelCredentials.newBuilder()
+                .trustManager(
+                        new X509TrustManager() {
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return null;
+                            }
+
+                            public void checkClientTrusted(
+                                    java.security.cert.X509Certificate[] certs,
+                                    String authType) {
+                            }
+
+                            public void checkServerTrusted(
+                                    java.security.cert.X509Certificate[] certs,
+                                    String authType) {
+                            }
+                        }
+                )
                 .keyManager(AGONES_CERTIFICATE_PATH.toFile(), AGONES_KEY_PATH.toFile())
                 .trustManager(this.caCertificate.getFile())
                 .build();
