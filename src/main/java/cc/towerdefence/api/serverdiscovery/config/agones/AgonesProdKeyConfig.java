8ce8315e2ea91efc373e761a5063225967d67898
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-10-27 03:57:51 +0100

    Apply previous to prod

--- src/main/java/cc/towerdefence/api/serverdiscovery/config/agones/AgonesProdKeyConfig.java
+++ src/main/java/cc/towerdefence/api/serverdiscovery/config/agones/AgonesProdKeyConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.security.Security;
@@ -26,6 +27,23 @@ public class AgonesProdKeyConfig {
     @Bean
     public ChannelCredentials sslContext() throws IOException {
         return TlsChannelCredentials.newBuilder()
+                .trustManager(
+                        new X509TrustManager() {
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return null;
+                            }
+
+                            public void checkClientTrusted(
+                                    java.security.cert.X509Certificate[] certs,
+                                    String authType) {
+                            }
+
+                            public void checkServerTrusted(
+                                    java.security.cert.X509Certificate[] certs,
+                                    String authType) {
+                            }
+                        }
+                )
                 .keyManager(AGONES_CERTIFICATE_PATH.toFile(), AGONES_KEY_PATH.toFile())
                 .trustManager(this.caCertificate.getFile())
                 .build();
