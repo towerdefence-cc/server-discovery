157694c13bf7cbf72620780dc617374a793128e3
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-11-10 06:30:02 +0000

    Add OTP server discovery

--- src/main/java/cc/towerdefence/api/serverdiscovery/controller/ServerOrchestratorController.java
+++ src/main/java/cc/towerdefence/api/serverdiscovery/controller/ServerOrchestratorController.java
@@ -1,8 +1,10 @@
 package cc.towerdefence.api.serverdiscovery.controller;
 
-import cc.towerdefence.api.model.server.ConnectableServerProto;
-import cc.towerdefence.api.model.server.LobbyServerProto;
 import cc.towerdefence.api.service.ServerDiscoveryGrpc;
+import cc.towerdefence.api.service.ServerDiscoveryProto;
+import cc.towerdefence.api.utils.utils.FunctionalFutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.Empty;
 import dev.agones.allocation.AllocationProto;
 import dev.agones.allocation.AllocationServiceGrpc;
@@ -13,6 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
+import java.util.concurrent.ForkJoinPool;
+
 
 @Controller
 @GrpcService
@@ -20,11 +24,11 @@ import org.springframework.stereotype.Controller;
 public class ServerOrchestratorController extends ServerDiscoveryGrpc.ServerDiscoveryImplBase {
     private static final Logger LOGGER = LoggerFactory.getLogger(ServerOrchestratorController.class);
 
-    private final AllocationServiceGrpc.AllocationServiceStub allocationServiceStub;
+    private final AllocationServiceGrpc.AllocationServiceFutureStub allocationService;
 
     @Override
-    public void getSuggestedLobbyServer(Empty request, StreamObserver<LobbyServerProto.LobbyServer> responseObserver) {
-        this.allocationServiceStub.allocate(AllocationProto.AllocationRequest.newBuilder()
+    public void getSuggestedLobbyServer(Empty request, StreamObserver<ServerDiscoveryProto.LobbyServer> responseObserver) {
+        ListenableFuture<AllocationProto.AllocationResponse> allocationFuture = this.allocationService.allocate(AllocationProto.AllocationRequest.newBuilder()
                 .setNamespace("towerdefence")
                 .addGameServerSelectors(
                         AllocationProto.GameServerSelector.newBuilder()
@@ -42,29 +46,61 @@ public class ServerOrchestratorController extends ServerDiscoveryGrpc.ServerDisc
                                 .setGameServerState(AllocationProto.GameServerSelector.GameServerState.READY)
                 )
                 .setScheduling(AllocationProto.AllocationRequest.SchedulingStrategy.Packed)
-                .build(), new StreamObserver<>() {
-            @Override
-            public void onNext(AllocationProto.AllocationResponse value) {
-                responseObserver.onNext(LobbyServerProto.LobbyServer.newBuilder()
-                        .setConnectableServer(
-                                ConnectableServerProto.ConnectableServer.newBuilder()
-                                        .setAddress(value.getAddress())
-                                        .setPort(value.getPortsList().get(0).getPort())
-                                        .setId(value.getGameServerName())
-                                        .build()
-                        ).build());
-            }
+                .build());
 
-            @Override
-            public void onError(Throwable throwable) {
-                LOGGER.error("Error getting LOBBY server: ", throwable);
-                responseObserver.onError(throwable);
-            }
+        Futures.addCallback(allocationFuture, FunctionalFutureCallback.create(
+                allocation -> {
+                    responseObserver.onNext(ServerDiscoveryProto.LobbyServer.newBuilder()
+                            .setConnectableServer(
+                                    ServerDiscoveryProto.ConnectableServer.newBuilder()
+                                            .setAddress(allocation.getAddress())
+                                            .setPort(allocation.getPortsList().get(0).getPort())
+                                            .setId(allocation.getGameServerName())
+                                            .build()
+                            ).build());
+                },
+                throwable -> {
+                    LOGGER.error("Error getting LOBBY server: ", throwable);
+                    responseObserver.onError(throwable);
+                }
+        ), ForkJoinPool.commonPool());
+    }
 
-            @Override
-            public void onCompleted() {
-                responseObserver.onCompleted();
-            }
-        });
+    @Override
+    public void getSuggestedOtpServer(Empty request, StreamObserver<ServerDiscoveryProto.ConnectableServer> responseObserver) {
+        ListenableFuture<AllocationProto.AllocationResponse> allocationFuture = this.allocationService.allocate(AllocationProto.AllocationRequest.newBuilder()
+                .setNamespace("towerdefence")
+                .addGameServerSelectors(
+                        AllocationProto.GameServerSelector.newBuilder()
+                                .putMatchLabels("agones.dev/fleet", "void-otp")
+                                .setGameServerState(AllocationProto.GameServerSelector.GameServerState.ALLOCATED)
+                                .setPlayers(
+                                        AllocationProto.PlayerSelector.newBuilder()
+                                                .setMinAvailable(1)
+                                                .setMaxAvailable(Integer.MAX_VALUE)
+                                                .build()
+                                )
+                ).addGameServerSelectors(
+                        AllocationProto.GameServerSelector.newBuilder()
+                                .putMatchLabels("agones.dev/fleet", "void-otp")
+                                .setGameServerState(AllocationProto.GameServerSelector.GameServerState.READY)
+                )
+                .setScheduling(AllocationProto.AllocationRequest.SchedulingStrategy.Packed)
+                .build());
+
+        Futures.addCallback(allocationFuture, FunctionalFutureCallback.create(
+                allocation -> {
+                    responseObserver.onNext(ServerDiscoveryProto.ConnectableServer.newBuilder()
+                            .setAddress(allocation.getAddress())
+                            .setPort(allocation.getPortsList().get(0).getPort())
+                            .setId(allocation.getGameServerName())
+                            .build()
+                    );
+                },
+                throwable -> {
+                    LOGGER.error("Error getting VOID-OTP server: ", throwable);
+                    responseObserver.onError(throwable);
+                }
+        ), ForkJoinPool.commonPool());
     }
 }
